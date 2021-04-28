87938487a84914787fd190504e42ab9d4881dcaf
Paulo Ricardo <palmeida.ipms@gmail.com>
2021-04-27 22:07:20 -0300

    Initialized repo

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true, // Ponto e virgula
+    trailingComma: 'all', // virgula no fim do objeto
+    // singleQuote: true, // Aspas simplese
+    printWidth: 80,
+    tabWidth: 4
+};
