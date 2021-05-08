2fb5852c47ba34ab6ccb7428fd56cfd2cd54927c
Paulo Ricardo <palmeida.ipms@gmail.com>
2021-05-08 13:18:10 -0300

    Modified configs

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -3,4 +3,6 @@ export const POSTS_URL = `${API_URL}/posts`;
 export const CATEGORIES_URL = `${API_URL}/categories`;
 
 export const SITE_NAME = "Paulo Ricardo";
-export const SITE_URL = process.env.SITE_URL || "http://localhost:3000";
+export const SITE_URL =
+    process.env.SITE_URL ||
+    "https://60969a762e0bb100073b4305--unruffled-newton-45299c.netlify.app";
